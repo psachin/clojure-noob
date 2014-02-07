27110f648d93cd3b4e3e8f9dc21a3df2319e4dbe
psachin <iclcoolster@gmail.com>
2014-02-07 13:45:15 +0530

    Subject: End of Section 3
    Description:
    
    http://www.braveclojure.com/do-things/

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -67,3 +67,170 @@
 
 (list 1 2 3 4)
 
+;;; add element at the begining of list
+(conj '(1 2 3) 4)
+
+;;;  ---- sets ----
+#{"hannan montana" "miley cyrus" 20 45}
+
+(set [3 3 3 4 4])
+
+;;; 3 exists in vector
+(get (set [3 3 3 4 4]) 3)
+
+;;; 5 exists in vector
+(get (set [3 3 3 4 4]) 5)
+
+;;; create a hash map and sorted map
+(hash-set 1 1 3 1 2)
+
+(sorted-set :b :a :c)
+
+
+
+;;; --- Symbols and Naming ---
+(def failed-movie-names
+  ["Gone with the moving air", "swellfellas"])
+
+;;; identity returns its argument
+(identity 'test)
+
+;;; --------------------
+;;; Calling functions
+(- 1 2 34)
+(first [1 2 3 4 5])
+
+(or + -)                                ;returns addition function
+((or + -) 1 2 3)                        ;lets add the list
+
+(and (= 1 1) 2)                         ;returns 2 if (= 1 1), else false
+((and (= 1 1) +) 1 2 3)                 ;if true, add the list
+
+((first [+ 0]) 1 2 3)
+
+(inc 1.1)                               ;increment function
+
+
+;;; --- Function parameters ----
+(defn no-param
+  []
+  "I take no parameters")
+
+(defn one-param
+  [x]
+  (str "I take one param: " x " It'd better ba a string."))
+
+(defn two-param
+  [x y]
+  (str "two param " x " " y))
+
+;;; --- multi arity function ---
+(defn multi-arity
+  ;; 3 arity argument and body
+  ([x y z]
+     (str x y z))
+  ([x y]
+     (str x y))
+  ([x]
+     (str x)))
+
+;;; rest-param function
+(defn codger-communication
+  [whippersnapper]
+  (str "Get of my lawn, " whippersnapper "!!!"))
+
+;;; now using rest-param(&)
+(defn codger
+  [& whippersnapper]
+  (map codger-communication whippersnapper))
+
+;;; mixing rest-param and normal-param
+(defn dodger
+  [name & habits]
+  (str "Hello " name ", You like " habits))
+
+(defn weird-arity
+  ([]
+     "some text")
+  ([number]
+     (inc number)))
+
+;;; returns the first element of a collection
+(defn my-first
+  [[first-thing]]; NOtice that first-thing is within a vector
+  first-thing)
+
+;; (my-first ["oven" "bike" "axe"])
+
+(defn my-other-first
+  [collection]
+  (first collection))
+
+(my-other-first ["nickel" "hair"])
+
+(defn announc-treasure-location
+  [{lat :lat lng :lng}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng)))
+
+(announc-treasure-location {:lat 28.22 :lng 81.33})
+
+;; Anonymous functions
+
+;; Anonymous function body
+;; (fn [param-list]
+;;   (function-body))
+
+(map (fn [name] (str "Hi, " name))
+     ["Dartg Vader" "He-man"])
+
+;; Another function
+((fn [x] (* x 3)) 8)
+
+;; Associate anonymous function with the name
+(def my-special-multiplier (fn [x] (* x 3)))
+;; (my-special-multiplier 5)
+
+;; More compact way to create anonymous functions
+#(* % 3)                                ;where '%' act as a variable.
+
+; (#(* % 3) 8)
+; (map #(* % 3) [1 2 3])
+
+;; Anonymous functions can get multiple arguments
+(#(str %1 " and " %2) "corn bread" "butter beans")
+
+;; If your anonymous function takes multiple arguments, you can
+;; distinguish them like this: %1, %2, %3, etc. % is equivalent to %1
+
+;; Pass a rest-param to anonymous function
+(#(identity %&) 1 "blarg" :yip)
+
+;;; --------------------
+;; Returning function
+(defn inc-maker
+  "Create a custom incrementor"
+  [inc-by]
+  #(+ % inc-by))
+
+(def inc3 (inc-maker 3))
+
+; (inc3 9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
